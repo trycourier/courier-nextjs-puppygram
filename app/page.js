d72eb91c69ed1790382c43e1a52e2ec1d07a9c63
Carter Rabasa <carter.rabasa@gmail.com>
2023-06-07 10:20:58 -0700

    added link to blog post

--- app/page.js
+++ app/page.js
@@ -18,6 +18,9 @@ export default async function Home(request) {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <Inbox userSignature={userSignature}/>
         </div>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a target="_new" href="https://courier.com/blog/introducing-puppygram-powered-by-courier-inbox-next-js-and-inngest">Check-out our blog post on how we build Puppygram</a>
+        </div>
       </div>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
