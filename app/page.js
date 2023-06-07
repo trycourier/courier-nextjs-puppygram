b0261ec7551a9e012fc12baca405c6db012a2221
Carter Rabasa <carter.rabasa@gmail.com>
2023-06-07 13:01:20 -0700

    tweaks

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import Inbox from './inbox'
 import crypto from 'crypto'
 
 async function getUserSignature() {
-  //console.log(process.env.COURIER_AUTH_TOKEN, process.env.NEXT_PUBLIC_COURIER_CLIENT_KEY)
   return crypto
     .createHmac("sha256", process.env.COURIER_AUTH_TOKEN)
     .update(process.env.NEXT_PUBLIC_COURIER_USER)
@@ -16,10 +15,10 @@ export default async function Home(request) {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <Inbox userSignature={userSignature}/>
+          <a target="_new" href="https://courier.com/blog/introducing-puppygram-powered-by-courier-inbox-next-js-and-inngest">Check-out our blog post on how we build Puppygram</a>
         </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a target="_new" href="https://courier.com/blog/introducing-puppygram-powered-by-courier-inbox-next-js-and-inngest">Check-out our blog post on how we build Puppygram</a>
+          <Inbox userSignature={userSignature}/>
         </div>
       </div>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
@@ -45,7 +44,7 @@ export default async function Home(request) {
         </div>
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="https://app.courier.com/?utm_source=courier-nextjs-password-reset&utm_medium=code-template&utm_campaign=devrel-apps"
+          href="https://app.courier.com/?utm_source=courier-puppygram&utm_medium=code-template&utm_campaign=devrel-apps"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -62,7 +61,7 @@ export default async function Home(request) {
         </a>
 
         <a
-          href="https://courier.com/docs?utm_source=courier-nextjs-password-reset&utm_medium=code-template&utm_campaign=devrel-apps"
+          href="https://courier.com/docs?utm_source=courier-puppygram&utm_medium=code-template&utm_campaign=devrel-apps"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -79,7 +78,7 @@ export default async function Home(request) {
         </a>
 
         <a
-          href="https://courier.com/integrations?utm_source=courier-nextjs-password-reset&utm_medium=code-template&utm_campaign=devrel-apps"
+          href="https://courier.com/integrations?utm_source=courier-puppygram&utm_medium=code-template&utm_campaign=devrel-apps"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -96,7 +95,7 @@ export default async function Home(request) {
         </a>
 
         <a
-          href="https://courier.com/changelog?utm_source=courier-nextjs-password-reset&utm_medium=code-template&utm_campaign=devrel-apps"
+          href="https://courier.com/changelog?utm_source=courier-puppygram&utm_medium=code-template&utm_campaign=devrel-apps"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
