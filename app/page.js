eb208ce9c40b64e2174aa78188a38edebe0c47b8
Carter Rabasa <carter.rabasa@gmail.com>
2023-06-07 13:15:09 -0700

    typo

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default async function Home(request) {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a target="_new" href="https://courier.com/blog/introducing-puppygram-powered-by-courier-inbox-next-js-and-inngest">Check-out our blog post on how we build Puppygram</a>
+          <a target="_new" href="https://courier.com/blog/introducing-puppygram-powered-by-courier-inbox-next-js-and-inngest">Check-out our blog post on how we built Puppygram</a>
         </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <Inbox userSignature={userSignature}/>
