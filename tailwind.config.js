4bf74ac0d28c6257ed0cdeb6d4084e6a1be8fbc7
Carter Rabasa <carter.rabasa@gmail.com>
2023-06-13 09:05:35 -0700

    added animation for hero image

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,21 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
+      },
+      animation: {
+        'waving': 'wave 2s linear infinite',
+      },
     },
   },
   plugins: [],
